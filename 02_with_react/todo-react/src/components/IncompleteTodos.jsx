05aa30cace3d92233b4e659361414f2647511a39
Miyazaki <mr.teruterubouz@gmail.com>
2023-01-06 23:30:49 +0900

    [todo-react] cssをjsxに移植

--- 02_with_react/todo-react/src/components/IncompleteTodos.jsx
+++ 02_with_react/todo-react/src/components/IncompleteTodos.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+const style = {
+  backgroundColor: "#c6ffe2",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
 export const IncompleteTodos = (props) => {
   const {todos, onClickDelete, onClickComplete} = props;
   return (
-    <div className='imcomplete-area'>
+    <div style={style}>
       <p className='title'>未完了のTODO</p>
       <ul>
         {todos.map((todo, index) => {
